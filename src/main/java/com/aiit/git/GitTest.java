86e39c4e9658ae2994761309536215e2f0488ec1
Lzc <1828121756@qq.com>
2023-01-06 20:09:39 +0800

    first commit

---
+++ src/main/java/com/aiit/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.aiit.git;
+
+/**
+ * @Author lzc
+ * @Date 2023/1/6 20:02
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
